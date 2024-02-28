24f74bb56f274e7b63b017f03f6aec8b16120bae
AliArif5781 <123650893+AliArif5781@users.noreply.github.com>
2024-02-28 10:51:19 +0500

    Props

--- 03tailwindProps/src/component/Card.jsx
+++ 03tailwindProps/src/component/Card.jsx
@@ -1,7 +1,7 @@
 // rfce likna sa line export thak like react functional component bana data ha.
 import React from 'react'
 
-function card({username,btnText="ALi"}) {   {/* inside card ({handling kehta ha}) aur yeh hmna destructing ki ha */}
+function card({username,btnText="ALi"}) {   {/* /* inside card ({handling kehta ha}) aur yeh hmna destructing ki ha.Props ko hata kr direct destructing kr di jati ha.direct hi apna username la liya in currly brasis {username} tu apko har jasa props.username ni likna para ga. direct username lijiya same hi kam hoga. */}
   // console.log("props",props);
   // console.log(props.username);This work when we write props in function paranthesis.Also a andothe method
   console.log(username);//  we can use this when we write username="e.t.c in App.jsx and we write inside function parathesis{username}=> username khu lika khu ki hmna App.jsx ma <Card username="Ali-Arif"/> card ma username likha ha is lya ."
@@ -20,7 +20,7 @@ function card({username,btnText="ALi"}) {   {/* inside card ({handling kehta ha}
       debitis?
     </p>
     <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      {btnText || "agr hmna value pass ni ki tu || lga kr jo pass krni ha wo likh do.React bolta"} →
+      {btnText || "agr hmna value pass ni ki tu || lga kr jo pass krni ha wo likh do.React bolta"} →  {/* agr hmna btnText App.jsx ma <Card/> component ma props pass ni kiya tu hm yaha pr || use kr ka value da skta ha in double comma "".But isma readability ka issue ata ha tu agr props ma value pass ni ki tu Card.jsx ma currly brasis ma value pass kr skta ha.*/}
     </button>
   </div>
 </div>
