01c0449067756a4c36e1a734a2ee99ce31d3bb2c
AliArif5781 <aliairf92@gmail.com>
2024-07-07 18:31:20 +0500

    React E_commerence Website

--- 14- ReactJs E_Commerence Website/v/src/components/Footer/Footer.jsx
+++ 14- ReactJs E_Commerence Website/v/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="container mx-auto text-center">
           <div className="flex justify-center items-center space-x-6 mb-4">
             <a
-              href="https://www.linkedin.com"
+              href="https://www.linkedin.com/in/ali-arif-140bb8274/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white hover:text-blue-300 transition duration-300 transform hover:scale-110"
@@ -16,7 +16,7 @@ const Footer = () => {
               <FaLinkedin size={30} />
             </a>
             <a
-              href="https://github.com"
+              href="https://github.com/AliArif5781/React-js"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white hover:text-gray-300 transition duration-300 transform hover:scale-110"
