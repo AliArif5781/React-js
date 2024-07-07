2eec45b695f2100a8aff7f82e53446fd356fda0b
AliArif5781 <aliairf92@gmail.com>
2024-07-07 18:28:11 +0500

    React E_commerence Website

--- 14- ReactJs E_Commerence Website/v/src/components/WomenProduct/WomenProduct.jsx
+++ 14- ReactJs E_Commerence Website/v/src/components/WomenProduct/WomenProduct.jsx
@@ -18,7 +18,7 @@ const WomenProduct = () => {
     <>
       <section className='py-16'>
         <div className="container mx-auto">
-          <h1 className='text-5xl text-center px-5 pb-7 text-indigo-500'>Our Product</h1>
+          <h1 className='text-5xl text-center py-5 md:py-0  text-indigo-500 font-semibold'>Our Product</h1>
           <SearchBtn setQuery={setQuery} />
           <div className='flex justify-center '>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px] mx-auto'>
